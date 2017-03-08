f1451b28b49782762f401b69a9e04f936cc11e90
冉光宇 <rgy@ranguangyudeMacBook-Pro.local>
2017-03-08 17:29:53 +0800

    rgy

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function process(source) {
-  return 'rgy test';
-}
-
 module.exports = function(source, map){
   this.cacheable && this.cacheable();
   //对source进行解析
-  var exports = process(source);
-  return "module.exports = " + exports;
+  return 'rgy';
 }
