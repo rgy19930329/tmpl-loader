f7712af457016990943cbf0f9d45511ab4d0fc44
冉光宇 <rgy@ranguangyudeMacBook-Pro.local>
2017-03-08 22:29:32 +0800

    rgy

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = function(source, map){
   this.cacheable && this.cacheable();
   //对source进行解析
-  return 'rgy';
+  var compiled = "var tpl = require('baidutemplate').template;\n";
+  compiled += "module.exports = tpl(";
+  compiled += JSON.stringify(source.toString('utf-8'));
+  compiled += ");";
+  return compiled;
 }
